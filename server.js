b3c2cada90e0df4d31bb082cbd9d174db49e6f82
Evan Cleary <evanjcleary@gmail.com>
2019-10-05 12:31:28 -0400

    test commit on master

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 var express = require("express");
 var exphbs = require("express-handlebars");
 var db = require("./models");
-// var mongojs = require("mongojs");
+var mongojs = require("mongojs");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
